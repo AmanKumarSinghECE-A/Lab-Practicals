05d77cf393fd77b06aeb12ba9f65858dea4bb59a
Aman Kumar Singh | ECE-A <149599521+AmanKumarSinghECE-A@users.noreply.github.com>
2023-12-24 13:59:59 +0530

    3_Sum_of_Natural_Numbers.c
    A c-positive integer n, write a program to calculate and return the sum of natural numbers up to n using loops.

--- PART 1/3_Sum_of_Natural_Numbers.c
+++ PART 1/3_Sum_of_Natural_Numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int sum_of_natural_numbers(int n)
+int sum_of_n_numbers(int n)
 {
   int sum = 0;
   for (int i = 1; i <= n; i++)
@@ -15,7 +15,7 @@ int main()
   int n, result;
   printf("Enter a positive integer: ");
   scanf("%d", &n);
-  result = sum_of_natural_numbers(n);
+  result = sum_of_n_numbers(n);
   printf("The sum of natural numbers up to %d is: %d", n, result);
   return 0;
 }
