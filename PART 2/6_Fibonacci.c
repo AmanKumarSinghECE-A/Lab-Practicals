41c876fe0d05800efe1e3cf50b8c1fb9f47caff7
Aman Kumar Singh | ECE-A <149599521+AmanKumarSinghECE-A@users.noreply.github.com>
2023-12-25 20:52:24 +0530

    Fibonacci Series

--- PART 2/6_Fibonacci.c
+++ PART 2/6_Fibonacci.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void printFibonacci(int n){
-static int n1=0,n2=1,n3;
-if(n>0){
-n3 = n1 + n2;
-n1 = n2;
-n2 = n3;
-printf("%d ",n3);
-printFibonacci(n-1);
+// Recursive function to return the nth Fibonacci number
+int fibonacci(int n) {
+// Base cases
+if (n == 0) {
+return 0;
 }
+if (n == 1) {
+return 1;
+}
+// Recursive case
+return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main(){
+int main() {
 int n;
-printf("Enter the number of elements: ");
-scanf("%d",&n);
-printf("Fibonacci Series: ");
-printf("%d %d ",0,1);
-printFibonacci(n-2);//n-2 because 2 numbers are already printed
+printf("Enter a positive integer: ");
+scanf("%d", &n);
+printf("The %dth Fibonacci number is %d\n", n, fibonacci(n));
 return 0;
 }
